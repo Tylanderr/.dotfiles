69b3d9a0998330ec61c4725fb4f8336e35663969
Tyler Harding <tyler.harding.sde@gmail.com>
2025-08-25 07:38:58 -0700

    indent ignore dbout, modified some tmux keybinds

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -95,7 +95,7 @@ return {
         enabled = false
       },
       filter = function(buf)
-        local exclude = { "java", "typescript", "markdown", "go", "lazy", "snacks_dashboard" }
+        local exclude = { "java", "typescript", "markdown", "go", "lazy", "snacks_dashboard", "dbout" }
         local ft = vim.bo[buf].filetype
         return not vim.tbl_contains(exclude, ft)
       end
