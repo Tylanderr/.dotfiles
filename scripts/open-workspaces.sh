b1e11208d8708a5a49ebe51d555b037e88bda24f
Tyler Harding <tyler.harding.sde@gmail.com>
2025-10-15 22:16:21 -0700

    fixed waybar, open workspaces script. setup workspace configs

--- scripts/open-workspaces.sh
+++ scripts/open-workspaces.sh
@@ -1,9 +1,8 @@
 # !/bin/bash
+hyprctl dispatch exec [workspace 6] $TERMINAL 
 hyprctl dispatch exec [workspace 2 silent] /opt/zen-browser-bin/zen
-hyprctl dispatch exec [workspace 3 silent] thunderbird
 hyprctl dispatch exec [workspace 5 silent] spotify
-hyprctl dispatch exec [workspace 6] ghostty 
-hyprctl dispatch workspace 6
+# hyprctl dispatch exec [workspace 3 silent] thunderbird
 
-sleep 2
+sleep 1
 hyprctl dispatch workspace 4 && hyprctl dispatch exec "omarchy-launch-or-focus-webapp Discord 'https://discord.com/channels/@me'"
