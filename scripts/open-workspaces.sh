813102b41b0d9d2043555f878bf6a179cb2d17e9
Tyler Harding <tyler.harding.sde@gmail.com>
2025-10-15 21:28:01 -0700

    waybar and workspaces in prog

--- scripts/open-workspaces.sh
+++ scripts/open-workspaces.sh
@@ -1,6 +1,9 @@
 # !/bin/bash
-hyprctl dispatch exec [workspace 2] /opt/zen-browser-bin/zen
+hyprctl dispatch exec [workspace 2 silent] /opt/zen-browser-bin/zen
 hyprctl dispatch exec [workspace 3 silent] thunderbird
 hyprctl dispatch exec [workspace 5 silent] spotify
-hyprctl dispatch exec [workspace 6] ghostty
-hyprctl dispatch exec "hyprctl dispatch workspace 4 && discord"
+hyprctl dispatch exec [workspace 6] ghostty 
+hyprctl dispatch workspace 6
+
+sleep 2
+hyprctl dispatch workspace 4 && hyprctl dispatch exec "omarchy-launch-or-focus-webapp Discord 'https://discord.com/channels/@me'"
