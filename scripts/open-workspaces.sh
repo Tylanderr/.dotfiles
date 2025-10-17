b14162368bd87812fe730e4e16724cd318a6b304
Tyler Harding <tyler.harding.sde@gmail.com>
2025-10-16 22:49:27 -0700

    setup windowrules for apps, disable open workspaces

--- scripts/open-workspaces.sh
+++ scripts/open-workspaces.sh
@@ -1,8 +1,1 @@
 # !/bin/bash
-hyprctl dispatch exec [workspace 6] $TERMINAL 
-hyprctl dispatch exec [workspace 2 silent] /opt/zen-browser-bin/zen
-hyprctl dispatch exec [workspace 5 silent] spotify
-# hyprctl dispatch exec [workspace 3 silent] thunderbird
-
-sleep 1
-hyprctl dispatch workspace 4 && hyprctl dispatch exec "omarchy-launch-or-focus-webapp Discord 'https://discord.com/channels/@me'"
