06954919519654e318067872cdc0d224af7112a1
Tyler Harding <tyler.harding.sde@gmail.com>
2025-12-16 09:42:11 -0800

    Update snacks.lua

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -110,5 +110,6 @@ return {
     { "<leader>fh",       function() Snacks.picker.help() end,        desc = "Help Pages" },
     { "<leader>fk",       function() Snacks.picker.keymaps() end,     desc = "Keymaps" },
     { "<leader>u",        function() Snacks.picker.undo() end,        desc = "Undo History" },
+    { "<leader>lg",       function() Snacks.lazygit() end,            desc = "Lazygit" },
   }
 }
