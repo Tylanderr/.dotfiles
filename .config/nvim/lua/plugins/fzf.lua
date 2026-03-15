5f0176b5fe12d97b233e5eab4bf8e48b5cb430e5
Tyler Harding <tyler.harding.sde@gmail.com>
2026-03-14 21:21:07 -0700

    cleanup snacks

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -6,6 +6,16 @@ return {
   ---@module "fzf-lua"
   ---@type fzf-lua.Config|{}
   ---@diagnostic disable: missing-fields
-  opts = {}
+  opts = {},
   ---@diagnostic enable: missing-fields
+  keys = {
+    { "<leader><leader>", function() require("fzf-lua").buffers() end,              desc = "Buffers" },
+    { "<leader>fg",       function() require("fzf-lua").live_grep() end,            desc = "Grep",                     nowait = true },
+    { "<leader>ff",       function() require("fzf-lua").files() end,                desc = "Find Files",               nowait = true },
+    { "<leader>fw",       function() require("fzf-lua").grep_visual() end,          desc = "Visual selection or word", mode = { "n", "x" } },
+    { "<leader>fd",       function() require("fzf-lua").diagnostics_document() end, desc = "Diagnostics" },
+    { "<leader>fh",       function() require("fzf-lua").helptags() end,             desc = "Help Pages" },
+    { "<leader>fk",       function() require("fzf-lua").keymaps() end,              desc = "Keymaps" },
+    { "<leader>u",        function() require("fzf-lua").undotree() end,             desc = "Undo History" },
+  },
 }
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,47 +1,3 @@
-local function palette_layout(height, width)
-  height = height or 0.4
-  width = width or 0.6
-  return {
-    preview = false,
-    layout = {
-      backdrop = false,
-      width = 0.5,
-      min_width = 80,
-      height = 0.6,
-      min_height = 3,
-      box = "vertical",
-      border = "rounded",
-      title = "{title}",
-      title_pos = "center",
-      { win = "input",   height = 1,          border = "bottom" },
-      { win = "list",    border = "none" },
-      { win = "preview", title = "{preview}", height = 0.4,     border = "top" },
-    },
-  }
-end
-
-local function files_layout(preview_width, height, width)
-  preview_width = preview_width or 0.6
-  height = height or 0.8
-  width = width or 0.8
-  return {
-    layout = {
-      box = 'horizontal',
-      width = width,
-      min_width = 120,
-      height = height,
-      {
-        box = 'vertical',
-        border = 'rounded',
-        title = '{title} {live} {flags}',
-        { win = 'input', height = 1,     border = 'bottom' },
-        { win = 'list',  border = 'none' },
-      },
-      { win = 'preview', title = '{preview}', border = 'rounded', width = preview_width },
-    },
-  }
-end
-
 return {
   "folke/snacks.nvim",
   priority = 1000,
@@ -58,9 +14,8 @@ return {
       enabled = true,
       preset = {
         keys = {
-          { icon = ' ', key = 'f', desc = 'File', action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = ' ', key = 'g', desc = 'Grep', action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = ' ', key = 'c', desc = 'Conf', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = ' ', key = 'f', desc = 'File', action = function() require("fzf-lua").files() end },
+          { icon = ' ', key = 'g', desc = 'Grep', action = function() require("fzf-lua").live_grep() end },
           { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
           { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
         }
@@ -80,18 +35,7 @@ return {
       }
     },
     picker = {
-      enabled = true,
-      sources = {
-        files = { layout = palette_layout(), hidden = true },
-        grep = { layout = files_layout(), hidden = true },
-        buffers = { layout = files_layout() },
-        grep_word = { layout = files_layout() },
-        diagnostics = { layout = files_layout() },
-        lsp_references = { layout = files_layout() },
-        help = { layout = { preset = "ivy" } },
-        keymaps = { layout = { preset = "ivy" } },
-        undo = { layout = { preset = "ivy" } },
-      },
+      enabled = false,
     },
     quickfile = { enabled = true },
     indent = {
@@ -108,14 +52,6 @@ return {
     },
   },
   keys = {
-    -- { "<leader><leader>", function() Snacks.picker.buffers() end, desc = "Buffers" },
-    -- { "<leader>fg",       function() Snacks.picker.grep() end,        desc = "Grep",                     nowait = true },
-    -- { "<leader>ff",       function() Snacks.picker.files() end,       desc = "Find Files",               nowait = true },
-    -- { "<leader>fw",       function() Snacks.picker.grep_word() end,   desc = "Visual selection or word", mode = { "n", "x" } },
-    -- { "<leader>fd",       function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
-    -- { "<leader>fh",       function() Snacks.picker.help() end,        desc = "Help Pages" },
-    -- { "<leader>fk",       function() Snacks.picker.keymaps() end,     desc = "Keymaps" },
-    -- { "<leader>u",        function() Snacks.picker.undo() end,    desc = "Undo History" },
     { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit" },
   }
 }
