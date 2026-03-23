0d66bd43d9c3041b557a814b5824182efc5e273f
Tyler Harding <tyler.harding.sde@gmail.com>
2026-03-23 06:54:26 -0700

    remove tokyo night theme changer, I don't use it

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -6,23 +6,10 @@ return {
     vim.cmd.hi 'Comment gui=none'
   end,
   config = function()
-    local current_scheme = 'tokyonight-storm'
     local current_transparency = true
 
     require('tokyonight').setup({
       transparent = current_transparency,
     })
-
-    -- Define toggle function
-    vim.keymap.set('n', '<leader>ut', function()
-      if current_scheme == 'tokyonight-storm' then
-        current_scheme = 'tokyonight-day'
-        current_transparency = false
-      else
-        current_scheme = 'tokyonight-storm'
-        current_transparency = true
-      end
-      vim.cmd.colorscheme(current_scheme)
-    end, { desc = 'Toggle Tokyonight Theme' })
   end
 }
