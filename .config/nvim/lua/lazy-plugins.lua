0168a07ec12c8d0c26a058a5f6305034b1c284dd
Tyler Harding <tyler.harding.sde@gmail.com>
2026-04-20 12:22:58 -0700

    remove hold, explicit import of plugins instead of using folder to designate enabled

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -1,6 +1,27 @@
 require('lazy').setup({
   spec = {
-    { import = "plugins" },
+    { import = "plugins.autopairs" },
+    { import = "plugins.blink" },
+    { import = "plugins.dadbod" },
+    { import = "plugins.diffview" },
+    { import = "plugins.fzf" },
+    { import = "plugins.harpoon" },
+    { import = "plugins.indent" },
+    { import = "plugins.java" },
+    { import = "plugins.lspconfig" },
+    { import = "plugins.lualine" },
+    { import = "plugins.mini" },
+    { import = "plugins.neogit" },
+    { import = "plugins.noice" },
+    { import = "plugins.oil" },
+    { import = "plugins.opencode" },
+    { import = "plugins.refactoring" },
+    { import = "plugins.tmux-navigator" },
+    { import = "plugins.todo-comments" },
+    { import = "plugins.tokyonight" },
+    { import = "plugins.treesitter" },
+    { import = "plugins.trouble" },
+    { import = "plugins.vimsleuth" },
   },
   ui = {
     border = "rounded"
