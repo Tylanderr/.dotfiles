3d11e6a63897416aa6623faf0373fb667a924135
Tyler Harding <tyler.harding.sde@gmail.com>
2026-07-29 10:07:41 -0700

    lazygit, no more neogit

---
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,20 @@
+return {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "tpope/vim-fugitive",
+    },
+
+    keys = {
+        { "<leader>gs", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        { "<leader>gb", "<cmd>Git blame<CR>", desc = "LazyGit" }
+    }
+}
